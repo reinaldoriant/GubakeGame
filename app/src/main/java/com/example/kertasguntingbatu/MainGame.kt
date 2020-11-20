8e7d2f9b650503874560e7b88295ed8a7bd80bfa
reinaldoriant <reinaldoriant@gmail.com>
2020-11-20 08:10:03 +0700

    First Blood

---
+++ app/src/main/java/com/example/kertasguntingbatu/MainGame.kt
@@ -0,0 +1,11 @@
+package com.example.kertasguntingbatu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mainGame)
+    }
+}
