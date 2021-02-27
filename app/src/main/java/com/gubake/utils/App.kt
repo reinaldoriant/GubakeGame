6052e92ce1968a9161f55f65ba503c6f10ea43c4
reinaldoriant <reinaldoriant@gmail.com>
2021-02-27 21:41:01 +0700

    add koin DI

--- app/src/main/java/com/gubake/utils/App.kt
+++ app/src/main/java/com/gubake/utils/App.kt
@@ -2,6 +2,14 @@ package com.gubake.utils
 
 import android.app.Application
 import android.content.Context
+import com.gubake.di.apiModule
+import com.gubake.di.dbModule
+import com.gubake.di.repositoryModule
+import com.gubake.di.uiModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.startKoin
 import java.lang.ref.WeakReference
 
 class App: Application() {
@@ -12,6 +20,10 @@ class App: Application() {
     override fun onCreate() {
         super.onCreate()
         context = WeakReference(applicationContext)
-
+        startKoin {
+            androidLogger()
+            context.get()?.let { androidContext(it) }
+            loadKoinModules(listOf(uiModule, repositoryModule, apiModule, dbModule))
+        }
     }
 }
